7bddca0939b1d96148384b48c16a4a9b1a68a101
raspirin <f1193968991@126.com>
2023-02-02 23:48:52 +0800

    new prepare_channel function

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,7 @@ fn main() -> Result<()> {
     // init basic context
     let default_config_path = "config.toml";
     let mut context = Context::new(default_config_path)?;
-
-    // read web rss
-    if let Some(uris) = context.config.uri {
-        for uri in uris {
-            let response = context.client.get(&uri).send()?;
-            let content = response.bytes()?;
-            let channel = Channel::read_from(&content[..])?;
-            context.channels.push(channel);
-        }
-    }
-    // read on disk rss
-    if let Some(files) = context.config.file {
-        for path in files {
-            let file = File::open(path)?;
-            let channel = Channel::read_from(BufReader::new(file))?;
-            context.channels.push(channel);
-        }
-    }
+    context.prepare_channels()?;
 
     for channel in context.channels {
         for item in channel.items {
