3d8b1a72cab723d08925a8550294ffbd5858e831
raspirin <f1193968991@126.com>
2023-02-03 13:52:44 +0800

    add episode list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,50 @@
 use anyhow::Result;
-use arni::Context;
+use arni::{Context, error::Error};
+
+struct Episode {
+    guid: String,
+    title: Option<String>,
+    torrent_link: String,
+}
+
+impl Episode {
+    fn new(guid: String, title: Option<String>, torrent_link: String) -> Self {
+        Self {
+            guid,
+            title,
+            torrent_link,
+        }
+    }
+}
+
+struct EpisodeList {
+    episodes: Vec<Episode>
+}
+
+impl EpisodeList {
+    fn new() -> Self {
+        Self {
+            episodes: vec![]
+        }
+    }
+
+    fn push(&mut self, item: &rss::Item) -> Result<()> {
+        let torrent_link = match item.enclosure() {
+            Some(enclosure) => enclosure.url().to_string(),
+            None => return Err(anyhow::Error::from(Error::BadTorrentLink)),
+        };
+        let guid = match item.guid() {
+            Some(guid) => guid.value().to_string(),
+            // if there is None, the function will return before this unwrap()
+            None => item.enclosure().unwrap().url().to_string(),
+        };
+        let title = item.title().map(|title| title.to_string());
+
+        let episode = Episode::new(guid, title, torrent_link);
+        self.episodes.push(episode);
+        Ok(())
+    }
+}
 
 fn main() -> Result<()> {
     // init basic context
@@ -7,9 +52,12 @@ fn main() -> Result<()> {
     let mut context = Context::new(default_config_path)?;
     context.prepare_channels()?;
 
+    // init items
+    let mut episodes = EpisodeList::new();
+
     for channel in context.channels.iter() {
         for item in channel.items().iter() {
-            println!("{}", item.enclosure().unwrap().url())
+            episodes.push(item)?;
         }
     }
 
