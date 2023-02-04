3d8880ca498268c1e16665694ccc67f8fbbd809f
raspirin <f1193968991@126.com>
2023-02-05 00:21:58 +0800

    remove useless struct Context

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,15 @@
 use anyhow::Result;
-use arni::{Context, error::Error};
-
-struct Episode {
-    guid: String,
-    title: Option<String>,
-    torrent_link: String,
-}
-
-impl Episode {
-    fn new(guid: String, title: Option<String>, torrent_link: String) -> Self {
-        Self {
-            guid,
-            title,
-            torrent_link,
-        }
-    }
-}
-
-struct EpisodeList {
-    episodes: Vec<Episode>
-}
-
-impl EpisodeList {
-    fn new() -> Self {
-        Self {
-            episodes: vec![]
-        }
-    }
-
-    fn push(&mut self, item: &rss::Item) -> Result<()> {
-        let torrent_link = match item.enclosure() {
-            Some(enclosure) => enclosure.url().to_string(),
-            None => return Err(anyhow::Error::from(Error::BadTorrentLink)),
-        };
-        let guid = match item.guid() {
-            Some(guid) => guid.value().to_string(),
-            // if there is None, the function will return before this unwrap()
-            None => item.enclosure().unwrap().url().to_string(),
-        };
-        let title = item.title().map(|title| title.to_string());
-
-        let episode = Episode::new(guid, title, torrent_link);
-        self.episodes.push(episode);
-        Ok(())
-    }
-}
+use arni::{error::Error, get_channels, get_episodes, init_client, init_config};
 
 fn main() -> Result<()> {
     // init basic context
     let default_config_path = "config.toml";
-    let mut context = Context::new(default_config_path)?;
-    context.prepare_channels()?;
+    let config = init_config(default_config_path);
+    let client = init_client();
 
-    // init items
-    let mut episodes = EpisodeList::new();
-
-    for channel in context.channels.iter() {
-        for item in channel.items().iter() {
-            episodes.push(item)?;
-        }
-    }
+    // basic loop
+    let channels = get_channels(&config, &client)?;
+    let episodes = get_episodes(&channels)?;
 
     Ok(())
 }
