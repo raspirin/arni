608a1aa16133fdc03613ec1542ef089a8426b611
raspirin <f1193968991@126.com>
2023-02-05 00:21:58 +0800

    remove useless struct Context

--- src/lib.rs
+++ src/lib.rs
@@ -4,96 +4,108 @@ use reqwest::blocking::Client;
 use rss::Channel;
 use std::fs::File;
 use std::io::BufReader;
+use crate::error::Error;
 
 pub mod config;
 pub mod history;
 pub mod jsonrpc;
 pub mod error;
 
-pub struct Context {
-    pub config: Config,
-    pub client: Client,
-    pub channels: Vec<Channel>,
+pub struct Episode {
+    pub guid: String,
+    pub title: Option<String>,
+    pub torrent_link: String,
 }
 
-impl Context {
-    pub fn new(config_path: &str) -> Result<Self> {
-        let config = Self::new_config(config_path);
-        let client = Self::new_client()?;
-
-        Ok(Self {
-            config,
-            client,
-            channels: vec![],
-        })
+impl Episode {
+    pub fn new(guid: String, title: Option<String>, torrent_link: String) -> Self {
+        Self {
+            guid,
+            title,
+            torrent_link,
+        }
     }
+}
 
-    fn new_config(path: &str) -> Config {
-        match config::load_config(path) {
-            Ok(config) => config,
-            Err(e) => panic!("Fail to load/create config. {e}"),
+pub fn init_config(config_path: &str) -> Config {
+    match config::load_config(config_path) {
+        Ok(config) => config,
+        Err(e) => panic!("Fail to load/create config. {e}"),
+    }
+}
+
+pub fn init_client() -> Client {
+    let user_agent = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+    match Client::builder().user_agent(user_agent).build() {
+        Ok(c) => c,
+        Err(e) => panic!("Fail to create a web client. {e}"),
+    }
+}
+
+pub fn get_channels(config: &Config, client: &Client) -> Result<Vec<Channel>> {
+    let mut ret: Vec<Channel> = vec![];
+
+    if let Some(uris) = &config.uri {
+        for uri in uris {
+            let channel = read_web_rss(uri, client)?;
+            ret.push(channel);
         }
     }
 
-    fn new_client() -> Result<Client> {
-        let user_agent = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
-        let client = match Client::builder().user_agent(user_agent).build() {
-            Ok(c) => c,
-            Err(e) => panic!("Fail to create a web client. {e}"),
-        };
-        Ok(client)
-    }
-
-    pub fn prepare_channels(&mut self) -> Result<()> {
-        if let Some(uris) = &self.config.uri {
-            for uri in uris {
-                let channel = self.read_web_rss(uri)?;
-                self.channels.push(channel);
-            }
+    if let Some(files) = &config.file {
+        for file in files {
+            let channel = read_on_disk_rss(file)?;
+            ret.push(channel);
         }
+    }
 
-        if let Some(files) = &self.config.file {
-            for file in files {
-                let channel = Self::read_on_disk_rss(file)?;
-                self.channels.push(channel);
-            }
+    Ok(ret)
+}
+
+fn read_web_rss(uri: &str, client: &Client) -> Result<Channel> {
+    let response = client.get(uri).send()?;
+    let content = response.bytes()?;
+
+    Ok(Channel::read_from(&content[..])?)
+}
+
+fn read_on_disk_rss(path: &str) -> Result<Channel> {
+    let file = File::open(path)?;
+
+    Ok(Channel::read_from(BufReader::new(file))?)
+}
+
+pub fn get_episodes(channels: &Vec<Channel>) -> Result<Vec<Episode>> {
+    let mut ret: Vec<Episode> = vec![];
+    for channel in channels {
+        for item in channel.items() {
+            push_episode(&mut ret, item)?;
         }
-
-        Ok(())
     }
 
-    fn read_web_rss(&self, uri: &str) -> Result<Channel> {
-        let response = self.client.get(uri).send()?;
-        let content = response.bytes()?;
-        Ok(Channel::read_from(&content[..])?)
-    }
+    Ok(ret)
+}
 
-    fn read_on_disk_rss(path: &str) -> Result<Channel> {
-        let file = File::open(path)?;
-        Ok(Channel::read_from(BufReader::new(file))?)
-    }
+fn push_episode(vec: &mut Vec<Episode>, item: &rss::Item) -> Result<()> {
+    let torrent_link = match item.enclosure() {
+        Some(enclosure) => enclosure.url().to_string(),
+        None => return Err(anyhow::Error::from(Error::BadTorrentLink)),
+    };
+    let guid = match item.guid() {
+        Some(guid) => guid.value().to_string(),
+        // if there is None, the function will return before this unwrap()
+        None => item.enclosure().unwrap().url().to_string(),
+    };
+    let title = item.title().map(|title| title.to_string());
+
+    let episode = Episode::new(guid, title, torrent_link);
+    vec.push(episode);
+
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_new_context() -> Result<()> {
-        let context = Context::new("empty_path")?;
-
-        assert_eq!(context.channels, vec![]);
-        assert_eq!(context.config.file, Some(vec![]));
-        assert_eq!(context.config.uri, Some(vec![]));
-        Ok(())
-    }
-
-    #[test]
-    fn test_prepare_empty_channel() -> Result<()> {
-        let mut context = Context::new("empty_path")?;
-        context.prepare_channels()?;
-
-        assert_eq!(context.channels, vec![]);
-        Ok(())
-    }
 }
