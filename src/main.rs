889381cba1f2a87267c09333e3f3857f5459be49
raspirin <f1193968991@126.com>
2023-02-05 14:26:20 +0800

    move some logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use anyhow::Result;
 use arni::history::History;
 use arni::persist::Persist;
-use arni::{error::Error, get_channels, get_episodes, init_client, init_config, Episode};
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::{Read, Write};
+use arni::{init_client, init_config, get_downloads};
 
 fn main() -> Result<()> {
     // init basic context
@@ -18,19 +14,13 @@ fn main() -> Result<()> {
     // basic loop
     let config = config.reload(default_config_path)?;
     let mut history = history.reload(default_history_path)?;
-    let channels = get_channels(&config, &client)?;
-    let episodes = get_episodes(&channels)?;
-    let mut to_download: Vec<Episode> = vec![];
-    for episode in episodes.into_iter() {
-        if !history.query_downloaded(&episode.guid) {
-            to_download.push(episode)
-        }
-    }
-    for episode in to_download.iter() {
-        println!("{:?}", history.query_downloaded(&episode.guid))
-    }
+
+    let _to_download = get_downloads(&client, &config, &mut history)?;
+
     config.write_to_disk(default_config_path)?;
     history.write_to_disk(default_history_path)?;
 
     Ok(())
 }
+
+
